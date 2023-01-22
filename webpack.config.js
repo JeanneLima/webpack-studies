ace2c531589249a6143195cbbd77d5bbf960f9e5
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-01-22 00:22:56 -0300

    chore(webpack): configure webpack to clean up dist folder before every build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = {
   entry: './app/src/js/app.js',  // Ponto de entrada (entry point) do código fonte da aplicação para o carregamento de todos os scripts (podendo ser módulos)
   output: { // informações dos arquivos de saída esperados unindo todos os scripts necessários para melhorar a métrica de latência (ou seja, menos requisições para carregar arquivos essenciais para a aplicação)
     filename: 'bundle.js', // Nome do arquivo
-    path: path.resolve(__dirname, 'app/dist') // Local de gravação que precisa ser um caminho absoluto, ou seja, desde a raiz do sistema operacional até a pasta desejada
+    path: path.resolve(__dirname, 'app/dist'), // Local de gravação que precisa ser um caminho absoluto, ou seja, desde a raiz do sistema operacional até a pasta desejada
+    clean: true // Opção para limpar pasta de output antes de cada build
   },
   plugins: [
     new HtmlWebpackPlugin({
