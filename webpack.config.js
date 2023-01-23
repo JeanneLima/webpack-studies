a20c6147ea8c2af3661546ec095347f1f64b266f
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-01-23 06:48:27 -0300

    chore(webpack): configure loaders in webpack to manage third-party library styling files as modules installed via NPM

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
  const path = require('path'); // Recurso do Node.js para retornar o caminho absoluto até o arquivo atual que funciona em qualquer sistema operacional
  const HtmlWebpackPlugin = require('html-webpack-plugin')
- const CopyWebpackPlugin = require('copy-webpack-plugin');
  
  // O arquivo de configuração do Webpack nada mais é do que um módulo do Node.js
  // É a plataforma Node.js que executa o Webpack através do sistema de módulos CommonJS
@@ -12,19 +11,24 @@ module.exports = {
     path: path.resolve(__dirname, 'app/dist'), // Local de gravação que precisa ser um caminho absoluto, ou seja, desde a raiz do sistema operacional até a pasta desejada
     clean: true // Opção para limpar pasta de output antes de cada build
   },
+  // Quem gerencia as dependências é o NPM, já o Webpack apenas permitirá adicioná-las ao bundle da aplicação tratando-os como módulos, contanto que haja um loader configurado para lidar com o tipo de arquivo em questão
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Tipo de arquivo a aplicar a regra (no caso, via expressão regular, todo arquivo encontrado que termine com extensão css, tendo a \ para fazer o escape do ponto)
+        use: [ // Lista de loaders baixados a serem usados para interpretar os arquivos apontados acima e inser-los no bundle do webpack
+        'style-loader', // Dinamicamente, em runtime, extrai o CSS gerado no arquivo de script do bundle e cria uma tag style para o mesmo no HTML  
+        'css-loader' // Permite importar css como módulo em arquivo Javascript, ou seja, faz com que o webpack aceite importar arquivos CSS através da instrução import e inclusive adiciona os CSS importados no bundle.js criado
+        ] 
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/src/app.html', // Arquivo de referência para que o plugin não crie um HTML novo do zero no build
       filename: 'app.html', // Nome do arquivo HTML a ser gerado no build
       hash: true // Opção para adicionar hash no arquivo do bundle a fim de invalidar cache quando houver alteração
     }),
-    new CopyWebpackPlugin({
-      patterns: [ // Propriedade que permite copiar uma lista de pastas
-        {
-          from: './app/src/css', // Caminho da pasta a ser copiada
-          to: 'css' // Caminho relativo na pasta de output definida para o Webpack
-        }
-      ]
-    })
-  ]
+  ],
 };
+
