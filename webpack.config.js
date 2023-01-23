01f3d9409866587d3de057980378105fd922a238
Jeanne Lima <jeannelima.profissional@gmail.com>
2023-01-23 06:06:50 -0300

    chore(webpack): configure webpack to copy css files to build package

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
  const path = require('path'); // Recurso do Node.js para retornar o caminho absoluto até o arquivo atual que funciona em qualquer sistema operacional
  const HtmlWebpackPlugin = require('html-webpack-plugin')
+ const CopyWebpackPlugin = require('copy-webpack-plugin');
  
  // O arquivo de configuração do Webpack nada mais é do que um módulo do Node.js
  // É a plataforma Node.js que executa o Webpack através do sistema de módulos CommonJS
@@ -16,6 +17,14 @@ module.exports = {
       template: './app/src/app.html', // Arquivo de referência para que o plugin não crie um HTML novo do zero no build
       filename: 'app.html', // Nome do arquivo HTML a ser gerado no build
       hash: true // Opção para adicionar hash no arquivo do bundle a fim de invalidar cache quando houver alteração
+    }),
+    new CopyWebpackPlugin({
+      patterns: [ // Propriedade que permite copiar uma lista de pastas
+        {
+          from: './app/src/css', // Caminho da pasta a ser copiada
+          to: 'css' // Caminho relativo na pasta de output definida para o Webpack
+        }
+      ]
     })
   ]
 };
